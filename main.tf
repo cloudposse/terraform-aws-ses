c6fdbf63b73e43eacb0685ffe3042d0f4c57dc5c
Marcin Brański <marcin@cloudposse.com>
2020-05-13 20:24:59 +0200

    Create valid DKIM verification record

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_route53_record" "amazonses_dkim_record" {
   count = var.enabled && var.verify_dkim ? 3 : 0
 
   zone_id = var.zone_id
-  name    = "${element(aws_ses_domain_dkim.ses_domain_dkim.0.dkim_tokens, count.index)}._domainkey.example.com"
+  name    = "${element(aws_ses_domain_dkim.ses_domain_dkim.0.dkim_tokens, count.index)}._domainkey.${var.domain}"
   type    = "CNAME"
   ttl     = "600"
   records = ["${element(aws_ses_domain_dkim.ses_domain_dkim.0.dkim_tokens, count.index)}.dkim.amazonses.com"]
