935bc2d41c59f11ff08744923d6228c5af653bae
Marcin Brański <marcin@cloudposse.com>
2020-05-13 21:50:53 +0200

    Rename output and add smtp credentials

--- docs/terraform.md
+++ docs/terraform.md
@@ -18,9 +18,9 @@
 
 | Name | Description |
 |------|-------------|
-| access_key_id | The access key ID |
-| secret_access_key | The secret access key. This will be written to the state file in plain-text |
-| user_arn | The ARN assigned by AWS for this user |
-| user_name | Normalized IAM user name |
-| user_unique_id | The unique ID assigned by AWS |
+| smtp_password | The SMTP password. This will be written to the state file in plain text. |
+| smtp_user | The SMTP user which is access key ID. |
+| user_arn | The ARN assigned by AWS for this user. |
+| user_name | Normalized IAM user name. |
+| user_unique_id | The unique ID assigned by AWS. |
 
--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,26 @@
 output "user_name" {
   value       = module.ses_user.user_name
-  description = "Normalized IAM user name"
+  description = "Normalized IAM user name."
 }
 
 output "user_arn" {
   value       = module.ses_user.user_arn
-  description = "The ARN assigned by AWS for this user"
+  description = "The ARN assigned by AWS for this user."
 }
 
 output "user_unique_id" {
   value       = module.ses_user.user_unique_id
-  description = "The unique ID assigned by AWS"
+  description = "The unique ID assigned by AWS."
 }
 
-output "access_key_id" {
-  value       = module.ses_user.access_key_id
-  description = "The access key ID"
-}
-
-output "secret_access_key" {
+# https://docs.aws.amazon.com/ses/latest/DeveloperGuide/smtp-credentials.html
+output "smtp_password" {
   sensitive   = true
-  value       = module.ses_user.secret_access_key
-  description = "The secret access key. This will be written to the state file in plain-text"
+  value       = module.ses_user.ses_smtp_password
+  description = "The SMTP password. This will be written to the state file in plain text."
+}
+
+output "smtp_user" {
+  value       = module.ses_user.access_key_id
+  description = "The SMTP user which is access key ID."
 }
