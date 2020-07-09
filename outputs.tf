df84d7f2458cf36d3feb14cb60eff5e6f9d76430
Marcin Brański <marcin@cloudposse.com>
2020-07-09 13:32:23 +0200

    Fix tests

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "ses_domain_identity_arn" {
-  value       = aws_ses_domain_identity.ses_domain.arn
+  value       = aws_ses_domain_identity.ses_domain.*.arn
   description = "The ARN of the SES domain identity."
 }
 
 output "ses_domain_identity_verification_token" {
-  value       = aws_ses_domain_identity.ses_domain.verification_token
+  value       = aws_ses_domain_identity.ses_domain.*.verification_token
   description = "A code which when added to the domain as a TXT record will signal to SES that the owner of the domain has authorised SES to act on their behalf. The domain identity will be in state 'verification pending' until this is done. See below for an example of how this might be achieved when the domain is hosted in Route 53 and managed by Terraform. Find out more about verifying domains in Amazon SES in the AWS SES docs."
 }
 
