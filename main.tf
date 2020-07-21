d9559b617025490f9d12558cf36a834946dc4c7c
John Shortland <JohnShortland@users.noreply.github.com>
2020-07-21 21:29:35 +0100

    Update main.tf
    added dnsimple

--- main.tf
+++ main.tf
@@ -39,6 +39,16 @@ resource "cloudflare_record" "amazonses_verification_record" {
   type    = "TXT"
   ttl     = "600"
 }
+  
+resource "dnsimple_record" "amazonses_verification_record" {
+  count = var.enabled && var.verify_dnsimple_domain ? 1 : 0
+  
+  domain  = var.domain
+  name    = "_amazonses.${var.domain}"
+  value   = aws_ses_domain_identity.ses_domain.0.verification_token
+  type    = "TXT"
+  ttl     = "600"
+}
 
 resource "aws_ses_domain_dkim" "ses_domain_dkim" {
   count = var.enabled ? 1 : 0
@@ -60,6 +70,20 @@ resource "cloudflare_record" "amazonses_dkim_record" {
   count = var.enabled && var.verify_cloudflare_dkim ? 3 : 0
 
   zone_id = var.zone_id
+  name = format(
+    "%s._domainkey.%s",
+    element(aws_ses_domain_dkim.ses_domain_dkim.0.dkim_tokens, count.index),
+    var.domain,
+  )
+  type    = "CNAME"
+  ttl     = "600"
+  value   = "${element(aws_ses_domain_dkim.ses_domain_dkim.0.dkim_tokens, count.index)}.dkim.amazonses.com"
+}
+  
+resource "dnsimple_record" "amazonses_dkim_record" {
+  count = var.enabled && var.verify_dnsimple_dkim ? 3 : 0
+
+  domain = var.domain
   name = format(
     "%s._domainkey.%s",
     element(aws_ses_domain_dkim.ses_domain_dkim.0.dkim_tokens, count.index),
