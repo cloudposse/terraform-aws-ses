1f566c25afe14f19997ba25aad8b6f9c5713bb83
Erik Osterman <erik@cloudposse.com>
2020-08-18 03:20:52 -0700

    Support terraform 0.13

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.10.0"
+  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.16.1"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
