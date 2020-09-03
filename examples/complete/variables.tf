903ab354707725151f08a9ec708fbb99f081a4e4
Andriy Knysh <aknysh@users.noreply.github.com>
2020-09-02 20:29:49 -0400

    Update to `context.tf` (#14)
    * Update to `context.tf`
    
    * Update to `context.tf`
    
    * Update to `context.tf`

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -3,27 +3,6 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "name" {
-  type        = string
-  description = "Name  (e.g. `app` or `cluster`)"
-}
-
-variable "namespace" {
-  type        = string
-  description = "Namespace (e.g. `eg` or `cp`)"
-}
-
-variable "stage" {
-  type        = string
-  description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
-}
-
-variable "environment" {
-  type        = string
-  default     = ""
-  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
-}
-
 variable "domain" {
   description = "The domain to create the SES identity for."
   type        = string
--- main.tf
+++ main.tf
@@ -1,27 +1,15 @@
-module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
-  enabled     = var.enabled
-  namespace   = var.namespace
-  environment = var.environment
-  stage       = var.stage
-  name        = var.name
-  delimiter   = var.delimiter
-  attributes  = var.attributes
-  tags        = var.tags
-}
-
 /*
 Create SES domain identity and verify it with Route53 DNS records
 */
 
 resource "aws_ses_domain_identity" "ses_domain" {
-  count = var.enabled ? 1 : 0
+  count = module.this.enabled ? 1 : 0
 
   domain = var.domain
 }
 
 resource "aws_route53_record" "amazonses_verification_record" {
-  count = var.enabled && var.verify_domain ? 1 : 0
+  count = module.this.enabled && var.verify_domain ? 1 : 0
 
   zone_id = var.zone_id
   name    = "_amazonses.${var.domain}"
@@ -31,13 +19,13 @@ resource "aws_route53_record" "amazonses_verification_record" {
 }
 
 resource "aws_ses_domain_dkim" "ses_domain_dkim" {
-  count = var.enabled ? 1 : 0
+  count = module.this.enabled ? 1 : 0
 
   domain = join("", aws_ses_domain_identity.ses_domain.*.domain)
 }
 
 resource "aws_route53_record" "amazonses_dkim_record" {
-  count = var.enabled && var.verify_dkim ? 3 : 0
+  count = module.this.enabled && var.verify_dkim ? 3 : 0
 
   zone_id = var.zone_id
   name    = "${element(aws_ses_domain_dkim.ses_domain_dkim.0.dkim_tokens, count.index)}._domainkey.${var.domain}"
@@ -51,16 +39,13 @@ resource "aws_route53_record" "amazonses_dkim_record" {
 Create user with permissions to send emails from SES domain
 */
 module "ses_user" {
-  source = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.15.0"
+  source = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.16.0"
 
-  enabled   = var.enabled
-  name      = module.label.name
-  namespace = var.namespace
-  stage     = var.stage
+  context = module.this.context
 }
 
 data "aws_iam_policy_document" "ses_user_policy" {
-  count = var.enabled ? 1 : 0
+  count = module.this.enabled ? 1 : 0
 
   statement {
     actions   = var.iam_permissions
@@ -69,9 +54,9 @@ data "aws_iam_policy_document" "ses_user_policy" {
 }
 
 resource "aws_iam_user_policy" "sending_emails" {
-  count = var.enabled ? 1 : 0
+  count = module.this.enabled ? 1 : 0
 
-  name   = module.label.id
+  name   = module.this.id
   policy = join("", data.aws_iam_policy_document.ses_user_policy.*.json)
   user   = module.ses_user.user_name
 }
