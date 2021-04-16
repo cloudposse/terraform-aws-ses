8edaf28b503a88511b09a424707c85e40bd83041
Frank <639906+syphernl@users.noreply.github.com>
2021-04-16 16:36:16 +0200

    fix: only create user when ses is enabled (#34)

--- main.tf
+++ main.tf
@@ -41,13 +41,13 @@ Create user with permissions to send emails from SES domain
 module "ses_user" {
   source  = "cloudposse/iam-system-user/aws"
   version = "0.20.2"
-  enabled = var.ses_user_enabled
+  enabled = (module.this.enabled && var.ses_user_enabled)
 
   context = module.this.context
 }
 
 data "aws_iam_policy_document" "ses_user_policy" {
-  count = var.ses_user_enabled ? 1 : 0
+  count = (module.this.enabled && var.ses_user_enabled) ? 1 : 0
 
   statement {
     actions   = var.iam_permissions
@@ -57,7 +57,7 @@ data "aws_iam_policy_document" "ses_user_policy" {
 
 resource "aws_iam_user_policy" "sending_emails" {
   #bridgecrew:skip=BC_AWS_IAM_16:Skipping `Ensure IAM policies are attached only to groups or roles` check because this module intentionally attaches IAM policy directly to a user.
-  count = var.ses_user_enabled ? 1 : 0
+  count = (module.this.enabled && var.ses_user_enabled) ? 1 : 0
 
   name   = module.this.id
   policy = join("", data.aws_iam_policy_document.ses_user_policy.*.json)
